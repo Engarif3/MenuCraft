5e258b709656ae6b705e993166a106d1bcbbb2b7
Md Arifur Rahman <arif.aust.eng@gmail.com>
2024-12-19 05:05:00 +0100

    ci cd yml added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,3 @@
-// module.exports = {
-//   mode: "jit",
-//   purge: [
-//     "./templates/**/*.html.twig", // Add any paths that Tailwind should scan for class names
-//     "./assets/**/*.js",
-//   ],
-//   darkMode: false, // Or 'media' or 'class'
-//   theme: {
-//     extend: {},
-//   },
-//   variants: {
-//     extend: {},
-//   },
-//   plugins: [require("daisyui")],
-// };
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
